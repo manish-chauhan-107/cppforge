02449aa2bd552e570ffa96d9d892ded9e434bd4b
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-05 17:11:09 +0530

    Organize C++ code by topics

---
+++ 05_Arrays/array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ int array[10] = {};
+ cout<<"enter 10 numbers : ";
+ for (int i =0 ; i < 10 ; i++){
+    cin >> array[i];
+ }
+for (int i =0 ; i < 10 ; i++){
+    cout<< array[i]<< "  ";
+}
+ return 0;
+}
