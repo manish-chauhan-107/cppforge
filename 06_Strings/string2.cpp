fd8791759a9bc1093cee3e6e3ee6c2c044b9b888
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-07 14:18:20 +0530

    Add string function getline , length , size

---
+++ 06_Strings/string2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main() {
+ string line ;
+ getline(cin , line);
+ cout<< "you enter :  " << line;
+ cout<<"\n";
+ cout<<line.length();
+ cout<<"\n";
+ cout<<line.size();
+ return 0;
+}
