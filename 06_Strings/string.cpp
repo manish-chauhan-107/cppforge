757a687b1c2064749c794723ff0b7d4263eb866c
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-07 14:16:04 +0530

    Add basic string function

---
+++ 06_Strings/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main() {
+ string name ;
+ cout<<"Enter string name :";
+ cin>> name;
+ cout<<"hello " <<name;
+ return 0;
+}
+// cin read only one word for multiple word we use "getline()"
