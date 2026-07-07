a7436d56636f103643523f543165de8852015892
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-07 14:00:21 +0530

    Add reverse array output without modifying original memory

---
+++ 05_Arrays/Reverse_array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ int arr[10] = {2,2,6,3,7,9,8,1,2,8};
+ for ( int i = 9 ; i >= 0 ; i--){
+    cout<<arr[i]<<" ";
+ }
+ return 0;
+}
