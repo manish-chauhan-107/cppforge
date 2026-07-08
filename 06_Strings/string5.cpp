947fe2ae43e50c04c9849979fd2b0740f3e27b74
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-08 12:18:07 +0530

    Add use of operator concatenate (join of two string)

---
+++ 06_Strings/string5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main() {
+ string frist_name = " MANISH";
+ string second_name = " CHAUHAN";
+ string name = frist_name + second_name ;
+ cout<< name ;
+ return 0;
+}
