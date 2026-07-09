9b5ea69d8ef33ad8b379290cb4e38ba09caabd5d
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-09 19:19:33 +0530

    Add another string in end by use append function

---
+++ 06_Strings/string6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main() {
+    string line1 = "HELLO ";
+    string line2 = "WORLD ";
+    line1.append(line2);
+    cout<< line1;
+ return 0;
+}
