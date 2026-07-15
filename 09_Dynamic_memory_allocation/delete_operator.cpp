a844d4550648743d32d581750637ec678ed15290
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-15 21:13:08 +0530

    Add delete operator work

---
+++ 09_Dynamic_memory_allocation/delete_operator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter size of dynamically allocated array"<<endl;
+    cin>>n;
+    int *arr = new int[n];
+    for (int i =0 ; i<n ; i++){
+        cout<<"enter element "<<(i+1)<<endl;
+        cin>>arr[i];
+    }
+    cout <<"Dynamically allocated array -- "<<endl ;
+    for (int i =0 ; i<n ; i++){
+        cout<<*(arr + i)<<" ";
+    }
+    cout<<"\n";
+    delete [] arr;
+    arr = nullptr;
+    if (arr == nullptr){
+        cout<<"Memory has been deleted. "<<endl;
+    }
+return 0;
+}
