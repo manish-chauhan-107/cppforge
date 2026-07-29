637dcf3d9d16984ba9e6415fa8fadd47ce47de72
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-29 19:48:09 +0530

    Basic of 2D vector

--- 10_STL/vector/2D_vector.cpp
+++ 10_STL/vector/2D_vector.cpp
@@ -1,39 +1,21 @@
-//
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
- int n;
- cout<<"enter number : ";
- cin>> n ;
- int a = n/20;
- if (a*20 == n){
-    cout <<"no of notes of 20 : "<< a <<endl;
+ vector<vector<int>> v(3 , vector<int>(4)); // here we make a vector of 3 row with each 4 element
+ for (int i = 0 ; i < 3 ; i++ ){
+    cout<<"Enter element of  row : "<<i+0 <<endl;
+    for (int j = 0 ; j < 4 ; j++){
+        cin>> v[i][j];
+    }
  }
- else{
-    int b = (n-(a*20))/10;
-    int m = (a*20)+(b*10);
-    if(m == n){
-        cout<< "no of notes of 20 : "<<a<<endl;
-        cout<< "no of notes of 10 : "<<b<<endl;
+ cout<<"The given metrix is : "<<endl;
+ for (int i = 0 ; i < 3 ; i++ ){
+    for (int j = 0 ; j < 4 ; j++){
+        cout<< v[i][j]<< " ";
     }
-    else{
-        int c = (n-m)/5;
-        int p = m + (c*5);
-        if(p == n){
-        cout<< "no of notes of 20 : "<<a<<endl;
-        cout<< "no of notes of 10 : "<<b<<endl;
-        cout<< "no of notes of 5 : "<<c<<endl;
-        }
-        else{
-            int d = (n-p);
-            cout<< "no of notes of 20 : "<<a<<endl;
-            cout<< "no of notes of 10 : "<<b<<endl;
-            cout<< "no of notes of 5 : "<<c<<endl;
-            cout<< "no of notes of 1 : "<<d<<endl;
-        }
-
-    }
+    cout<<endl;
  }
  return 0;
 }
