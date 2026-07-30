7242c7b72bd1f85df36f719ba4d950b5e3af1f2e
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-30 22:58:45 +0530

    Add decleration & excess of pair

---
+++ 10_STL/Pair/basic_of_pair.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    pair<string , int> data = {"Manish chauhan" , 19}; // syntex of decleration
+    cout<<"Name : " <<data.first<<endl;  // excess of elements
+    cout<<"Age : " <<data.second<<endl;
+ return 0;
+}
