cca4797be0c7ec54378b40abf35128496fcbc2d7
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-07-31 11:32:34 +0530

    To be continue

---
+++ 10_STL/Set/set_basic.cpp
@@ -0,0 +1,15 @@
+/*A set is a container that stores only unique 
+elements in sorted (ascending) order. Duplicate element are remove by itself*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ set <string> name ;
+ //Function for give input in set
+ name.insert("OG Panther");
+ name.insert("Ruhul Amin");
+ name.insert("Vishnu kant rai");
+ name.insert("OG yugesh");
+ name.insert("Aniket kumar singh");
+ return 0;
+}
